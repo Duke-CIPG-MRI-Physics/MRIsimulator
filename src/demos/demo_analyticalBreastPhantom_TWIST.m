34e8097952a12cb4756ee342fa2764588523d548
Roberto Carrascosa <robie.carrascosa@gmail.com>
2026-01-23 00:31:51 -0500

    adding oversampling and %resolution

--- src/demos/demo_analyticalBreastPhantom_TWIST.m
+++ src/demos/demo_analyticalBreastPhantom_TWIST.m
@@ -3,32 +3,20 @@ close all;
 clc; 
 
 %% 1) FOV and matrix size (scanner-style inputs)
-FOV_mm = [600 600 600];
-FOV_mm = [400 600 350]; %TODO: something still not right here, not centered?
-N      = [224 224 224]; % [Nx Ny Nz]
-resolution = FOV_mm./N;
+freq_phase_slice = [3 2 1]; % 1 = R/L, 2=A/P, 3 = S/I 
+%TODO: ask Scott about this ^
+
+
+
+%load('Breast_Ultrafast_scan_parameters.mat') %TODO: figure out why this %isn't working
+load('test_scan_parameters.mat')
+[FOV_mm,Matrix_size,voxel_size_mm,nominal_resolution_mm] = get_true_kspace_size(test_scan_parameters);
+
+%TODO: make sure the output is correct and dimension order is correct
+
+resolution = FOV_mm./Matrix_size;
 resolution_normalized = resolution/max(resolution);
 
-fprintf('Voxel size: [%.2f %.2f %.2f] mm\n', resolution(1), resolution(2), resolution(3));
-fprintf('Grid size : %d x %d x %d\n', N(1), N(2), N(3));
-
-%% 2) Configure acquisition ordering and timing
-freq_phase_slice = [3 2 1]; % 1 = R/L, 2=A/P, 3 = S/I
-% [kOrderedIdx, ~] = orderRectilinearKspace(N, freq_phase_slice, 1, 1);
-
-% parameters from clinical ultrafast protocol
-FOV_read_mm = 350;
-FOV_phase_pct = 100;
-oversampling_phase_pct = 20; 
-oversampling_slice_pct = 33.3; 
-slices_per_slab = 240;
-slice_thickness_mm = 1; % Note this is the reconstructed slice thickness, not the nominal slice thickness 
-base_resolution = 224;
-phase_resolution_pct = 100;
-slice_resolution_pct = 80;
-freq_phase_slice = [3 2 1]; % 1 = R/L, 2=A/P, 3 = S/I
-
-% TODO - interpolation is off in current protocol, but we could consider it as an option in the future...
 
 % Contrast parameters
 
@@ -37,21 +25,23 @@ TR = (5.88E-3);
 TE = 2.63E-3;
 
 % Derived contrast paramters
-rBW_Hz = rBW_HzPerPix*base_resolution;
+rBW_Hz = rBW_HzPerPix*Matrix_size(1);  
 dt_s = 1/rBW_Hz;   % dwell time between frequency-encode samples [s]
+
+%% 2) Configure acquisition ordering and timing
 
 
 pA = 0.05;
 Nb = 10;
 Time_Measured = 300; %sec
-R = 1;
-PF_Factor = 1;
+R = 1; %[2 3]
+PF_Factor = 1; %[6/8 6/8]
 
-Sampling_Table = Ultrafast_Sampling(N,pA,Nb,Time_Measured,TR,R,PF_Factor);
+Sampling_Table = Ultrafast_Sampling(Matrix_size,pA,Nb,Time_Measured,TR,R,PF_Factor);
 
 kOrderedIdx = [Sampling_Table.Frequency, Sampling_Table.("Row (phase)"), Sampling_Table.("Column (slice)")];
 
-
+%TODO: Understand the loop below
 encodingDimStr = {'freq:',', phase:',', slice:'};
 encodingFullStr = '';
 for iDim = 1:3
@@ -71,7 +61,7 @@ disp(encodingFullStr)
 
 %% 3) Build WORLD k-space grid and map to the TWIST ordering
 disp('Building WORLD k-space')
-[kx_vec, ky_vec, kz_vec, kx, ky, kz] = computeKspaceGrid3D(FOV_mm, N);
+[kx_vec, ky_vec, kz_vec, kx, ky, kz] = computeKspaceGrid3D(FOV_mm, Matrix_size); %TODO: This might be causing error due to zero padding, not sure it's being accounted for
 
 kx_orderedIdx = kOrderedIdx(:, 1);
 ky_orderedIdx = kOrderedIdx(:, 2);
@@ -82,9 +72,9 @@ ordKsx_ky = ky_vec(ky_orderedIdx);
 ordKsx_kz = kz_vec(kz_orderedIdx);
 
 %% 5) Construct the breast phantom with the embedded enhancing vessel
-t_PE = TR-(N(1)*dt_s); %TODO: rename these variables for clarity
+t_PE = TR-(Matrix_size(1)*dt_s); %TODO: rename these variables for clarity
 t_s = t_PE+dt_s:dt_s:TR;
-multipliers = 1:height(Sampling_Table)/N(1);
+multipliers = 1:height(Sampling_Table)/Matrix_size(1);
 
 matrix_result = t_s(:) + (multipliers * TR);
 
@@ -103,7 +93,7 @@ K_ordered = phantom.kspace(ordKsx_kx(sortIdx), ordKsx_ky(sortIdx), ordKsx_kz(sor
 
 Sampling_Table.Kspace_Value = K_ordered';
 
-TWISTed_Kspace = zeros([N,max(Sampling_Table.Bj)+1]);
+TWISTed_Kspace = zeros([Matrix_size,max(Sampling_Table.Bj)+1]);
 TWISTed_Kspace(Sampling_Table.("Linear Index")) = Sampling_Table.Kspace_Value;
 %% ---  6. Updating K-Space from each measurement to undo TWIST
 
