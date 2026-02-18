4633ff92484e52893b8f7fd04c7b8b251cf221d8
Roberto Carrascosa <robie.carrascosa@gmail.com>
2026-02-18 16:40:42 -0500

    Update Ultrafast_Sampling.m

--- src/Simulator/Ultrafast_Sampling.m
+++ src/Simulator/Ultrafast_Sampling.m
@@ -1,8 +1,33 @@
-function [Sampling_Table,TWIST_Stats] = Ultrafast_Sampling(Matrix_Size_Acquired,FOV_acquired,pA,pB,N_Measurements,TR,R,PF_Factor)
+function [Complete_Sampling_Table,TWIST_Stats] = Ultrafast_Sampling(Matrix_Size_Acquired,FOV_acquired,pA,pB,N_Measurements,TR,R,PF_Factor)
 %This function implements full sampling trajectory for Ultrafast MRI
 %Imaging using TWIST,GRAPPA, and Partial Fourier
-%   GRAPPA and PF are optional input. Due to MATLAB limitations, the way to
-%   skip one or both inputs is to use a value of [1,1]
+
+
+%The inputs of this function are:
+
+% Matrix_Size_Acquired --- vector of form: [#frequency, #phase (rows), #slice (columns)]
+
+% FOV_acquired --- vector of form: [freq FOV, phase FOV, slice FOV] 
+%   units do not matter
+
+% pA --- defines the size of the central region that is sampled for
+%   every measurement
+
+% pB --- where pB defines the fraction of the exteriorvof k-space which is 
+%   sampled every measurement. There is a limited set of allowed inputs
+
+% N_Measurements --- the number of timepoints/frames after the initial full
+% k-space acquisition to collect
+
+% TR --- MRI repetion time, time required to collect a single line of
+% k-space
+
+% R --- GRAPPA acceleration factor of form [phase, slice]
+%   can be skipped with value of 1
+
+% PF_Factor --- Partial Fourier acceleration factor of form [phase fraction, slice fraction]
+%   can be skipped with value of 1
+
 
 %Compared to Timing Demo, this function does not output to command line,
 %passes as output
@@ -45,13 +70,15 @@ N = round(1/pB);
 Sampling_Table = TWIST(pA,pB,Matrix_Size_Acquired,FOV_acquired,R,PF_Factor);
 
 %% --- Undersampling
+% Strategy is to simply remove entries which correspond with the 2
+% techniques
 
-if all(R ~= 1)
+if any(R ~= 1)
     %GRAPPA
     Sampling_Table = GRAPPA_Undersample(Matrix_Size_Acquired,Sampling_Table,R);
 end
 
-if all(PF_Factor ~= 1)
+if any(PF_Factor ~= 1)
     %Partial Fourier
     Sampling_Table = PF_Undersample(Matrix_Size_Acquired,Sampling_Table,PF_Factor);
 end
@@ -59,16 +86,21 @@ end
 
 %% --- Correcting for number of measurements 
 
-Sampling_Table_A = Sampling_Table(Sampling_Table.Bj == 0, :);
-Sampling_Table_B = Sampling_Table(Sampling_Table.Bj ~= 0, :);
+Sampling_Table_initial = Sampling_Table(Sampling_Table.Frame == 0,:);
 
-Complete_Sampling_Table = Sampling_Table;
-Complete_Sampling_Table.Bj(:) = 0;
+Sampling_Table_A = Sampling_Table(Sampling_Table.Frame == 0 & Sampling_Table.Bj == 0,:);
+Sampling_Table_B = Sampling_Table(Sampling_Table.Frame == 0 & Sampling_Table.Bj ~= 0,:);
+
+
+
+Complete_Sampling_Table = Sampling_Table_initial;
 
 frame_number = 1;
 i_Bj = 1;
 while frame_number <= N_Measurements 
     Frame_Sampling_Table = [Sampling_Table_A;Sampling_Table_B(Sampling_Table_B.Bj==i_Bj,:)];
+    Frame_Sampling_Table.Frame = frame_number * ones(height(Frame_Sampling_Table),1);
+
     Complete_Sampling_Table = [Complete_Sampling_Table;Frame_Sampling_Table];
     
     frame_number = frame_number + 1;
@@ -79,75 +111,42 @@ while frame_number <= N_Measurements
     end
 end
 
-% % 2. Build the full periodic table in a single, vectorized operation
-% if N_Measurements > 0 && ~isempty(Sampling_Table_B)
-%     % Replicate the base table 'N_Measurements' times using repmat
-%     Sampling_Table_B_Replicated = repmat(Sampling_Table_B, N_Measurements, 1);
-% 
-%     % Create a column vector of offsets to add (e.g., [0; N; 2*N; ...])
-%     offsets = (0:N_Measurements-1)' * N;
-% 
-%     % Use repelem to expand the offsets to match the replicated table's size
-%     bj_offsets = repelem(offsets, height(Sampling_Table_B), 1);
-% 
-%     % Add the offsets to the Bj column all at once
-%     Sampling_Table_B_Replicated.Bj = Sampling_Table_B_Replicated.Bj + bj_offsets;
-% 
-%     % Remove rows that exceed the total number of measurements
-%     Sampling_Table_B_Replicated(Sampling_Table_B_Replicated.Bj > Num_Measurements, :) = [];
-% else
-%     % Handle case where no repetitions are needed or Table B is empty
-%     Sampling_Table_B_Replicated = Sampling_Table_B;
-%     Sampling_Table_B_Replicated(Sampling_Table_B_Replicated.Bj > Num_Measurements, :) = [];
-% end
-% 
-% % 3. Combine the static part with the new periodic part
-% Sampling_Table = [Sampling_Table_A; Sampling_Table_B_Replicated];
 
-%% --- Calculating Actual Time
-Num_Measurements_Actual = Num_Measurements;
-Complete_Sampling_Table = renamevars(Complete_Sampling_Table,"Bj","Frame");
+%% --- Calculating Timing Stats
+Preparation_Scan_Time = TR*sum(Sampling_Table.Frame == 0);
+Measurement_Time = TR*sum(Complete_Sampling_Table.Frame ~= 0);
+Temporal_Resolution = Measurement_Time/N_Measurements;
 
-
-
-%% --- Calculating Time
-Temporal_Resolution_Actual = TR*sum(Sampling_Table.Bj ~= 0)/Num_Measurements_Actual;
-Preparation_Scan_Time_Actual = TR*sum(Sampling_Table.Bj == 0);
-Measurement_Time_Actual = Num_Measurements_Actual*Temporal_Resolution_Actual;
-
-Timing_Actual = table(Temporal_Resolution_Actual,Num_Measurements_Actual,Preparation_Scan_Time_Actual,Measurement_Time_Actual);
-Timing_Actual.Properties.VariableNames = {'Average Temporal Resolution (s)','# of Measurements','Preparation Scan Time (s)','Measurement Time (s)'};
-Timing_Actual.Properties.RowNames = {'Actual Scan Timing'};
-
-disp(Timing_Actual)
+TWIST_Stats = table(Preparation_Scan_Time,Temporal_Resolution,Measurement_Time);
+TWIST_Stats.Properties.VariableNames = {'Preparation Scan Time (s)','Average Temporal Resolution (s)','Measurement Time (s)'};
 
 %% --- Incorporate Frequency Encoding
 %This can take place at the very end as all we are doing is duplicating
 %each entry by the number of Frequency encodes, this does not impact the
 %calculation of scan time
-
-N_freqs = Matrix_Size_Acquired(1); % We want to duplicate each row 3 times
-
-% Step 1: Expand the table
-% We generate a list of indices where each index is repeated N times
-expandedIdx = repelem(1:height(Sampling_Table), N_freqs);
-
-original_height = height(Sampling_Table);
-
-Sampling_Table = Sampling_Table(expandedIdx, :);
-
-% Step 2: Add the repetition counter
-% We generate a vector [1; 2; 3] and stack it for every original row
-Sampling_Table.Frequency = repmat((1:N_freqs)', original_height, 1);
-
-%Correct linear index column for addded dimension
-% Order: [Frequency, Phase, Slice, Frame]
-sz_4D = [Matrix_Size_Acquired(1), Matrix_Size_Acquired(2), Matrix_Size_Acquired(3), max(Sampling_Table.Bj)+1];
-
-Sampling_Table.("Linear Index") = sub2ind(sz_4D, ...
-    Sampling_Table.Frequency, ...          % Dim 1
-    Sampling_Table.("Row (phase)"), ...    % Dim 2
-    Sampling_Table.("Column (slice)"), ... % Dim 3
-    Sampling_Table.Bj + 1);                % Dim 4
+% 
+% N_freqs = Matrix_Size_Acquired(1); 
+% 
+% % Step 1: Expand the table
+% % We generate a list of indices where each index is repeated N times
+% expandedIdx = repelem(1:height(Complete_Sampling_Table), N_freqs);
+% 
+% original_height = height(Complete_Sampling_Table);
+% 
+% Complete_Sampling_Table = Complete_Sampling_Table(expandedIdx, :);
+% 
+% % Step 2: Add the repetition counter
+% % We generate a vector [1; 2; 3] and stack it for every original row
+% Complete_Sampling_Table.Frequency = repmat((1:N_freqs)', original_height, 1);
+% 
+% %Correct linear index column for addded dimension
+% % Order: [Frequency, Phase, Slice, Frame]
+% sz_4D = [Matrix_Size_Acquired(1), Matrix_Size_Acquired(2), Matrix_Size_Acquired(3), max(Complete_Sampling_Table.Bj)+1];
+% 
+% Complete_Sampling_Table.("Linear Index") = sub2ind(sz_4D, ...
+%     Complete_Sampling_Table.Frequency, ...          % Dim 1
+%     Complete_Sampling_Table.("Row (phase)"), ...    % Dim 2
+%     Complete_Sampling_Table.("Column (slice)"), ... % Dim 3
+%     Complete_Sampling_Table.Bj + 1);                % Dim 4
 
 end
